06c2878ef3a4415bd195c224bdb1160d94c94d72
Bành Hảo Toàn <86637323+mjkundta@users.noreply.github.com>
2023-03-27 18:56:12 +0700

    Features - Login, Register
    Login and Register features (without SMS authentication)

---
+++ app/src/main/java/com/groupb/locationsharing/RegisterActivity.java
@@ -0,0 +1,152 @@
+package com.groupb.locationsharing;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText username, email, password;
+    Button btn_register;
+    TextView returnLogin;
+    FirebaseAuth auth;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn_register = findViewById(R.id.btn_register);
+        returnLogin = findViewById(R.id.returnLogin);
+
+        returnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        auth = FirebaseAuth.getInstance();
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username_Txt = String.valueOf(username.getText());
+                String email_Txt = String.valueOf(email.getText());
+                String password_Txt = String.valueOf(password.getText());
+
+                if(TextUtils.isEmpty(username_Txt) || TextUtils.isEmpty(password_Txt)
+                || TextUtils.isEmpty(email_Txt)){
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "All field must be filled",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if (!is_Valid_Password(password_Txt)) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Invalid password",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    register(username_Txt, password_Txt, email_Txt);
+                }
+            }
+        });
+    }
+
+    private void register(String username, String password, String email){
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            assert firebaseUser != null;
+                            String userid = firebaseUser.getUid();
+
+                            reference = FirebaseDatabase.getInstance()
+                                    .getReference("Users").child(userid);
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username);
+                            hashMap.put("imageUrl", "default");
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                        }
+                        else{
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "Your email is invalid or being used",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    //Check password valid
+
+    public static boolean is_Valid_Password(String password) {
+
+        if (password.length() < 6) return false;
+
+        int charCount = 0;
+        int numCount = 0;
+        for (int i = 0; i < password.length(); i++) {
+
+            char ch = password.charAt(i);
+
+            if (is_Numeric(ch)) numCount++;
+            else if (is_Letter(ch)) charCount++;
+            else return false;
+        }
+
+
+        return (charCount >= 2 && numCount >= 2);
+    }
+
+    public static boolean is_Letter(char ch) {
+        ch = Character.toUpperCase(ch);
+        return (ch >= 'A' && ch <= 'Z');
+    }
+
+
+    public static boolean is_Numeric(char ch) {
+
+        return (ch >= '0' && ch <= '9');
+    }
+}
